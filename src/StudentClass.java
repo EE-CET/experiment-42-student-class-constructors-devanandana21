433578e32a8f90e0fab3125e41031fddbb955019
devanandana21 <tve24el018@cet.ac.in>
2026-02-06 10:59:22 +0530

    Update StudentClass.java

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -5,7 +5,6 @@ public class StudentClass {
     String name;
     int rollNo;
 
-    // Parameterized constructor
     StudentClass(String name, int rollNo) {
         this.name = name;
         this.rollNo = rollNo;
@@ -18,20 +17,17 @@ public class StudentClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input for student 1
         String name1 = sc.nextLine();
         int roll1 = sc.nextInt();
-        sc.nextLine(); // consume newline
+        sc.nextLine(); 
 
-        // Input for student 2
         String name2 = sc.nextLine();
         int roll2 = sc.nextInt();
 
-        // Create objects
         StudentClass s1 = new StudentClass(name1, roll1);
         StudentClass s2 = new StudentClass(name2, roll2);
 
-        // Output (EXACT format)
+    
         s1.display();
         s2.display();
 
