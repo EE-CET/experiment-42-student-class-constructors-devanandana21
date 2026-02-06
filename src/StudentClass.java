78bd75c52b3d1a00877fc988ffca5e04ae8d0a5d
devanandana21 <tve24el018@cet.ac.in>
2026-02-06 10:10:54 +0530

    Update StudentClass.java

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -11,29 +11,29 @@ public class StudentClass {
         this.rollNo = rollNo;
     }
 
-    void display(int studentNumber) {
-        System.out.println("Student " + studentNumber + " details: Name: " + name + " Roll: " + rollNo);
+    void display() {
+        System.out.println("Name: " + name + " Roll: " + rollNo);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Student 1 input
+        // Input for student 1
         String name1 = sc.nextLine();
         int roll1 = sc.nextInt();
-        sc.nextLine(); // consume leftover newline
+        sc.nextLine(); // consume newline
 
-        // Student 2 input
+        // Input for student 2
         String name2 = sc.nextLine();
         int roll2 = sc.nextInt();
 
-        // Object creation
+        // Create objects
         StudentClass s1 = new StudentClass(name1, roll1);
         StudentClass s2 = new StudentClass(name2, roll2);
 
-        // Output
-        s1.display(1);
-        s2.display(2);
+        // Output (EXACT format)
+        s1.display();
+        s2.display();
 
         sc.close();
     }
